a10769d5e00ee1c1e8124e8d7b156b94ea2145f9
Christine Nia <christinenia@me.com>
2018-12-06 15:35:17 -0800

    theme change

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,7 +10,4 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
-  // app.get("*", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/home.html"));
-  // });
 };
