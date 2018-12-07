d737414a1e52c266016fea775a2f35f52f1b591f
Christine Nia <christinenia@me.com>
2018-12-06 19:23:23 -0800

    updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,103 @@
-var friendsArray = {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
+var friends = [
+    {
+      name: "Jim Halpert",
+      photo: "http://www.pajiba.com/assets_c/2018/05/jim-halpert-revisionism-thumb-700xauto-196874.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
       ]
-  };
+    },
+    {
+      name: "Pam Beesly",
+      photo: "https://theofficeanalytics.files.wordpress.com/2017/09/pam-halpert-1.jpg?w=1024",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Michael Scott",
+      photo: "https://i.amz.mshcdn.com/r59uAtQeFkaZXdJ9GvLRVWUXzno=/950x534/filters:quality(90)/https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com%2Fuploads%2Fcard%2Fimage%2F732905%2Fd0fb8eb6-894d-4942-94db-4d86cc2ef241.jpg",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Dwight Schrute",
+      photo: "https://curiousgeorge2015.files.wordpress.com/2016/02/rainn-wilson-dwight-schrute-the-office2.jpg",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Angela Kinsey",
+      photo: "https://img2.thejournal.ie/inline/4187026/original/?width=620&version=4187026",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Kelly Kapoor",
+      photo: "https://imgix.bustle.com/elite-daily/2016/03/06153729/the-office_R0DHKR.jpg?w=945&h=574&fit=crop&crop=faces&auto=format&q=70",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
   
-
-  module.exports = friendsArray;
+  ];
+  
+  // exports the friends array
+  module.exports = friends;
+  
--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-var express = require("express");
-var path = require("path");
+// Dependencies
+var express = require('express');
+var path = require('path');
 
-// Sets up the Express App
-// =============================================================
+// Configure the Express application
 var app = express();
 var PORT = process.env.PORT || 8000;
 
-var router = express.Router();
+// Expose the public directory to access CSS files
+app.use(express.static(path.join(__dirname, './app/public')));
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+// Add middleware to parse incoming request bodies
 app.use(express.json());
-app.use(express.static(path.join(__dirname + "/app/public")));
+app.use(express.urlencoded({ extended: true })); //security feature for server
 
-
+// Application routes
+//switchboard to routes
+//unnessary if its small app and routing between files isn't necessary
 require(path.join(__dirname, './app/routing/apiRoutes'))(app);
 require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
-
+// Start listening on PORT
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+  console.log('App is listening on PORT: ' + PORT);
+});
